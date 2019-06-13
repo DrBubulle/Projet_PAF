9f94e67a93705612e1f26f46a5a803670a8d54f6
Gander <victor.gander@gmail.com>
2019-06-13 16:52:00 +0200

    first commit

---
+++ Projet_PAF/src/main/java/com/inti/formation/ProjetPafApplication.java
@@ -0,0 +1,13 @@
+package com.inti.formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetPafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetPafApplication.class, args);
+	}
+
+}
