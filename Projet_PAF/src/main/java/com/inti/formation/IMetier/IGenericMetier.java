21eeae6f9a2d74ba3451eae23b211e5bce134f20
Gander <victor.gander@gmail.com>
2019-06-14 11:40:23 +0200

    classe Utilisateur et IGenericMetier

--- Projet_PAF/src/main/java/com/inti/formation/IMetier/IGenericMetier.java
+++ Projet_PAF/src/main/java/com/inti/formation/IMetier/IGenericMetier.java
@@ -1,5 +1,12 @@
 package com.inti.formation.IMetier;
 
-public interface IGenericMetier {
+import java.util.List;
 
+public interface IGenericMetier<A,I> {
+
+    public A ajouter(A a);
+    public A update(A a);
+    public void delete (I id);
+    public A findOne(I id);
+    public List<A> findAll();
 }
--- Projet_PAF/src/main/java/com/inti/formation/Model/Utilisateur.java
+++ Projet_PAF/src/main/java/com/inti/formation/Model/Utilisateur.java
@@ -1,13 +1,74 @@
 package com.inti.formation.Model;
 
-public class Utilisateur {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Utilisateur implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	
-	private int IdUtilisateur;
+	@Id
+	private long IdUtilisateur;
 	private String email;
 	private String nom;
 	private String prenom;
-	private String testmaryssa;
+	
+	public Utilisateur() {
+		super();
+	}
+
+	public Utilisateur(long idUtilisateur, String email, String nom, String prenom) {
+		super();
+		IdUtilisateur = idUtilisateur;
+		this.email = email;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+
+	public long getIdUtilisateur() {
+		return IdUtilisateur;
+	}
+
+	public void setIdUtilisateur(long idUtilisateur) {
+		IdUtilisateur = idUtilisateur;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	@Override
+	public String toString() {
+		return "Utilisateur [IdUtilisateur=" + IdUtilisateur + ", email=" + email + ", nom=" + nom + ", prenom="
+				+ prenom + "]";
+	}
+	
 	
 
 	
