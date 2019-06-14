072b8b75eb85ba3eeff8639fb85793220541d83a
Arassym <maryssa.semer@hotmail.fr>
2019-06-14 11:48:56 +0200

    Classe Affaire et Phase - Maryssa

--- Projet_PAF/src/main/java/com/inti/formation/Model/Affaire.java
+++ Projet_PAF/src/main/java/com/inti/formation/Model/Affaire.java
@@ -1,5 +1,101 @@
 package com.inti.formation.Model;
 
-public class Affaire {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Affaire implements Serializable {
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private long idAffaire;
+	private String reference;
+	private String titre;
+	private String description;
+	private String status;
+	
+	//Constructeurs
+	public Affaire() {
+		super();
+	}
+
+
+	public Affaire(long idAffaire, String reference, String titre, String description, String status) {
+		super();
+		this.idAffaire = idAffaire;
+		this.reference = reference;
+		this.titre = titre;
+		this.description = description;
+		this.status = status;
+	}
+
+	//Getters and Setters
+	public long getIdAffaire() {
+		return idAffaire;
+	}
+
+
+	public void setIdAffaire(long idAffaire) {
+		this.idAffaire = idAffaire;
+	}
+
+
+	public String getReference() {
+		return reference;
+	}
+
+
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+
+
+	public String getTitre() {
+		return titre;
+	}
+
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public String getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	//toString
+	@Override
+	public String toString() {
+		return "Affaire [idAffaire=" + idAffaire + ", reference=" + reference + ", titre=" + titre + ", description="
+				+ description + ", status=" + status + "]";
+	}
+	
+	
+	
+	
+	
 
 }
--- Projet_PAF/src/main/java/com/inti/formation/Model/Phase.java
+++ Projet_PAF/src/main/java/com/inti/formation/Model/Phase.java
@@ -1,5 +1,66 @@
 package com.inti.formation.Model;
 
-public class Phase {
+import java.io.Serializable;
+
+public class Phase implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private long idPhase;
+	private String nom;
+	private String dateDebut;
+	private String dateFin;
+	
+	//cosntructeurs
+	public Phase() {
+		super();
+	}
+	public Phase(long idPhase, String nom, String dateDebut, String dateFin) {
+		super();
+		this.idPhase = idPhase;
+		this.nom = nom;
+		this.dateDebut = dateDebut;
+		this.dateFin = dateFin;
+	}
+	
+	//Getters and Setters
+	public long getIdPhase() {
+		return idPhase;
+	}
+	public void setIdPhase(long idPhase) {
+		this.idPhase = idPhase;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getDateDebut() {
+		return dateDebut;
+	}
+	public void setDateDebut(String dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+	public String getDateFin() {
+		return dateFin;
+	}
+	public void setDateFin(String dateFin) {
+		this.dateFin = dateFin;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	//toString
+	@Override
+	public String toString() {
+		return "Phase [idPhase=" + idPhase + ", nom=" + nom + ", dateDebut=" + dateDebut + ", dateFin=" + dateFin + "]";
+	}
+	
+	
 
 }
