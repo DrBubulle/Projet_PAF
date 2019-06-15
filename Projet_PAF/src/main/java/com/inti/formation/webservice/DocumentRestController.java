8f6338051fad56d3b2ef6f7ad6e631fe4dc22227
Gander <victor.gander@gmail.com>
2019-06-15 15:19:03 +0200

    webservice Utilisateur et Document -Victor

--- Projet_PAF/src/main/java/com/inti/formation/webservice/DocumentRestController.java
+++ Projet_PAF/src/main/java/com/inti/formation/webservice/DocumentRestController.java
@@ -1,5 +1,55 @@
 package com.inti.formation.webservice;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.inti.formation.IMetier.IDocumentMetier;
+import com.inti.formation.Model.Document;
+
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping(value="/apiDocument")
 public class DocumentRestController {
 
+	@Autowired
+	private IDocumentMetier metier;
+
+	public IDocumentMetier getMetier() {
+		return metier;
+	}
+
+	public void setMetier(IDocumentMetier metier) {
+		this.metier = metier;
+	}
+	@RequestMapping(value="/ajouterDoc", method=RequestMethod.POST)
+	public Document ajouter (@RequestBody Document d) {
+		return metier.ajouter(d);
+			}
+	
+	@RequestMapping(value="/updateDoc", method=RequestMethod.PUT)
+	public Document update(@RequestBody Document d) {
+		return metier.update(d);
+	}
+	@RequestMapping(value="deleteDoc/{idDocument}", method=RequestMethod.DELETE)
+	public void delete(@PathVariable("idDocument")long idDocument) {
+		metier.delete(idDocument);
+	}
+	
+	@RequestMapping(value="/document/{idDocument}",method=RequestMethod.GET)
+	public Document findOne (@PathVariable("idDocument")long idDocument) {
+		return metier.findOne(idDocument);
+	}
+	
+	@RequestMapping(value="/documents", method=RequestMethod.GET)
+	public List<Document> findAll(){
+		return metier.findAll();
+	}
 }
--- Projet_PAF/src/main/java/com/inti/formation/webservice/UtilisateurRestController.java
+++ Projet_PAF/src/main/java/com/inti/formation/webservice/UtilisateurRestController.java
@@ -1,5 +1,57 @@
 package com.inti.formation.webservice;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.inti.formation.IMetier.IUtilisateurMetier;
+import com.inti.formation.Model.Utilisateur;
+
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping(value="/apiUtilisateur")
 public class UtilisateurRestController {
 
+	@Autowired
+	private IUtilisateurMetier metier;
+
+	public IUtilisateurMetier getMetier() {
+		return metier;
+	}
+
+	public void setMetier(IUtilisateurMetier metier) {
+		this.metier = metier;
+	}
+	
+	@RequestMapping(value="/ajouterUt", method=RequestMethod.POST)
+	public Utilisateur ajouter (@RequestBody Utilisateur u) {
+		return metier.ajouter(u);
+	}
+	
+	@RequestMapping(value="/updateUt", method=RequestMethod.PUT)
+	public Utilisateur update (@RequestBody Utilisateur u) {
+		return metier.update(u);
+	}
+	
+	@RequestMapping(value="deleteUt/{idUtilisateur}", method=RequestMethod.DELETE)
+	public void delete(@PathVariable("idUtilisateur")long idUtilisateur) {
+		metier.delete(idUtilisateur);
+	}
+	
+	@RequestMapping(value="/utilisateur/{idUtilisateur}",method=RequestMethod.GET)
+	public Utilisateur findOne (@PathVariable("idUtilisateur")long idUtilisateur) {
+		return metier.findOne(idUtilisateur);
+	}
+	
+	@RequestMapping(value="/utilisateurs", method=RequestMethod.GET)
+	public List<Utilisateur> findAll(){
+		return metier.findAll();
+	}
 }
