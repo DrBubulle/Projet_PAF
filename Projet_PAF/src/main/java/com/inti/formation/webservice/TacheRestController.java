a1d5f91bad33e2e0cd261dbdff28aa0d7401b79b
Laetoria <pruvostlaetoria@yahoo.com>
2019-06-16 12:42:41 +0200

    Ecriture WebService Tribunal + Tache

--- Projet_PAF/src/main/java/com/inti/formation/webservice/TacheRestController.java
+++ Projet_PAF/src/main/java/com/inti/formation/webservice/TacheRestController.java
@@ -1,5 +1,60 @@
 package com.inti.formation.webservice;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.inti.formation.IMetier.ITacheMetier;
+import com.inti.formation.Model.Tache;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/apiTache")
 public class TacheRestController {
 
+	private ITacheMetier tachemetier;
+
+	public ITacheMetier getTachemetier() {
+		return tachemetier;
+	}
+	public void setTachemetier(ITacheMetier tachemetier) {
+		this.tachemetier = tachemetier;
+	}
+	
+	@RequestMapping(value="/ajouterTache", method=RequestMethod.POST)
+	public Tache ajouter(@RequestBody Tache t) {
+		return tachemetier.ajouter(t);
+	}
+	
+	@RequestMapping(value="/updateTache", method=RequestMethod.PUT)
+	public Tache update(@RequestBody Tache t) {
+		return tachemetier.update(t);
+	}
+	
+	@RequestMapping(value="/deleteTache/{id}", method=RequestMethod.DELETE)
+	public void delete(@PathVariable("id") long id) {
+		tachemetier.delete(id);
+	}
+	
+	@RequestMapping(value="/tache/{id}", method=RequestMethod.GET)
+	public Tache findOne(@PathVariable("id") long id) {
+		return tachemetier.findOne(id);
+	}
+	
+	@RequestMapping(value="/taches", method=RequestMethod.GET)
+	public List<Tache> findAll(){
+		return tachemetier.findAll();
+	}
+	
+	
+	
+	
+	
+	
+	
 }
--- Projet_PAF/src/main/java/com/inti/formation/webservice/TribunalRestController.java
+++ Projet_PAF/src/main/java/com/inti/formation/webservice/TribunalRestController.java
@@ -1,5 +1,55 @@
 package com.inti.formation.webservice;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.inti.formation.IMetier.ITribunalMetier;
+import com.inti.formation.Model.Tribunal;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/apiTribunal")
 public class TribunalRestController {
 
+	private ITribunalMetier tribunalmetier;
+
+	
+	public ITribunalMetier getTribunalmetier() {
+		return tribunalmetier;
+	}
+	public void setTribunalmetier(ITribunalMetier tribunalmetier) {
+		this.tribunalmetier = tribunalmetier;
+	}
+	
+	@RequestMapping(value="/ajouterTribunal", method=RequestMethod.POST)
+	public Tribunal ajouter(@RequestBody Tribunal t) {
+		return tribunalmetier.ajouter(t);
+	}
+	
+	@RequestMapping(value="/updateTribunal", method=RequestMethod.PUT)
+	public Tribunal update(@RequestBody Tribunal t) {
+		return tribunalmetier.update(t);
+	}
+	
+	@RequestMapping(value="/deleteTribunal/{id}", method=RequestMethod.DELETE)
+	public void delete(@PathVariable("id") long id) {
+		tribunalmetier.delete(id);
+	}
+	
+	@RequestMapping(value="/tribunal/{id}", method=RequestMethod.GET)
+	public Tribunal findOne(@PathVariable("id") long id) {
+		return tribunalmetier.findOne(id);
+	}
+	
+	@RequestMapping(value="/tribunaux", method=RequestMethod.GET)
+	public List<Tribunal> findAll(){
+		return tribunalmetier.findAll();
+	}
+	
 }
